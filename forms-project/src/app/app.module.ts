aeafa1b7547ef67cc2a01c2ec3a663ef1d710984
Enes Kirimi <enesmil@gmail.com>
2018-02-05 17:28:24 +0100

    Added the demo form SKU

---
+++ forms-project/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormSkuComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
